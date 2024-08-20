68590236ff5e27463b3a74b4a7c00aaec6505888
Whittun <whittun@gmail.com>
2024-08-20 12:52:02 +0300

    feat: get all data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,114 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+interface LanguageNode {
+  node: {
+    name: string;
+  };
 }
 
-export default App;
+interface NodeData {
+  name: string;
+  description: string;
+  primaryLanguage: {
+    name: string;
+  };
+  languages: {
+    edges: LanguageNode[];
+  };
+  forkCount: number;
+  stargazerCount: number;
+  updatedAt: string;
+}
+
+interface Edge {
+  node: NodeData;
+}
+
+interface RepositoryData {
+  name: string;
+  description: string;
+  primaryLanguage: string;
+  languages: LanguageNode[];
+  forkCount: number;
+  stargazerCount: number;
+  updatedAt: string;
+}
+
+const query = `query {
+  search(query: "test", type: REPOSITORY, first: 10) {
+    edges {
+      node {
+        ... on Repository {
+          name
+          description
+          primaryLanguage {
+            name
+          }
+          languages(first:10) {
+            edges {
+              node {
+                name
+              }
+            }
+          }
+          forkCount
+          stargazerCount
+          updatedAt
+        }
+      }
+    }
+  }
+}`;
+
+const getGithubData = async () => {
+  const response = await fetch("https://api.github.com/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `bearer ${process.env.REACT_APP_API_KEY}`,
+    },
+    body: JSON.stringify({ query }),
+  });
+
+  const data = await response.json();
+
+  const formattedData = data.data.search.edges.map(({ node }: Edge) => {
+    const {
+      name,
+      description,
+      primaryLanguage: { name: primaryLanguage },
+      languages,
+      forkCount,
+      stargazerCount,
+      updatedAt,
+    } = node;
+
+    return {
+      name,
+      description,
+      primaryLanguage,
+      languages,
+      forkCount,
+      stargazerCount,
+      updatedAt,
+    };
+  });
+  console.log(formattedData);
+  return formattedData;
+};
+
+export const App = () => {
+  const [githubData, setGithubData] = useState<RepositoryData[] | null>(null);
+
+  const setFetchData = async () => {
+    const githubData = await getGithubData();
+
+    setGithubData(githubData);
+  };
+
+  useEffect(() => {
+    setFetchData();
+  }, []);
+
+  return <div>{githubData && githubData.map((elem) => elem.name)}</div>;
+};
